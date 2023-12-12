1f7eafa6440977103eca85b9bcf3cdd5096efb36
Chun-Wei, Mo <chunwei.mo@gmail.com>
2023-12-11 20:51:44 -0800

    Create a helper function sum_of_integers

--- coding_question/process_reading.py
+++ coding_question/process_reading.py
@@ -1,18 +1,23 @@
 def process_readings(file_name):
     try:
         with open(file_name) as file_object:
-            file = file_object.readlines()
+            integer_list = file_object.readlines()
     except FileNotFoundError:
         print(f'{file_name} does not exist.')
-        print(f'Program stops')
     else:
-        sum_of_integer = 0
-        for integer in file:
-            sum_of_integer += int(integer)
-    
+        sum_of_integer = sum_of_integers(integer_list)
         with open(file_name, 'a') as file_object:
             file_object.write(str(sum_of_integer))
+            
+    
         
+def sum_of_integers(integer_list):
+    sum_of_integer = 0
+    for integer in integer_list:
+        sum_of_integer += int(integer)
+    return sum_of_integer
+
+
 def main():
     process_readings('process_reading.txt')
     
